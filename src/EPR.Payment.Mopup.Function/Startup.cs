40f4c94a77b44cde051daa56c82776d8059f598d
Rekha Devi <rekha.devi.external@eviden.com>
2025-05-09 10:48:29 +0100

    added scret for function app

--- src/EPR.Payment.Mopup.Function/Startup.cs
+++ src/EPR.Payment.Mopup.Function/Startup.cs
@@ -3,11 +3,10 @@ using EPR.Payment.Mopup.Function;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.ApplicationInsights;
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Reflection;
 
 
 [assembly: FunctionsStartup(typeof(Startup))]
@@ -28,6 +27,31 @@ namespace EPR.Payment.Mopup.Function
                 .AddEnvironmentVariables()
                 .Build();
             builder.Services.AddDataContext(config, config["SqlConnectionString"]);
+        }
+
+        /// <summary>
+        /// Configures the application's configuration sources for an Azure Functions app.
+        /// This method overrides the default configuration setup to include:
+        /// - The current directory as the base path
+        /// - User secrets (useful for local development and secure storage of sensitive data)
+        /// - Environment variables (for runtime configuration across environments)
+        /// 
+        /// This setup allows the function app to access configuration values from multiple sources,
+        /// supporting both local development and cloud deployment scenarios.
+        /// </summary>
+        /// <param name="builder">
+        /// The <see cref="IFunctionsConfigurationBuilder"/> used to customize the configuration pipeline.
+        /// </param>
+
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            var builtConfig = builder.ConfigurationBuilder.Build();
+
+            builder.ConfigurationBuilder
+               .SetBasePath(Environment.CurrentDirectory)
+               .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+               .AddEnvironmentVariables()
+               .Build();
         }
     }
 }
